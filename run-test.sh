6262f79e1f287a957cc5d8b7cf4ce4e5e8afea01
Luca Castoro <castoro.luc@gmail.com>
2019-03-08 04:20:39 +0100

    Improved the run-test.sh script.

--- run-test.sh
+++ run-test.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+TEMPLOG=/tmp/jsuidbfuia.log
+
+function at_exit {
+  rm -f $TEMPLOG
+}
+
+trap "rm -f $TEMPLOG" EXIT
+
 ROOT=$(dirname $(realpath $0))
 
 # build or update the image
-docker build -t ssh-server $ROOT &>/dev/null
+docker build -t ssh-server $ROOT &>$TEMPLOG || {
+  echo 'Image build failed:'
+  cat $TEMPLOG
+  exit 1
+}
 
 # run the container (detached)
-docker run --rm -d -h test -p 2222:22 ssh-server &>/dev/null
+docker run --rm -d -h test -p 2222:22 ssh-server &>$TEMPLOG || {
+  echo 'Could not start the container:'
+  cat $TEMPLOG
+  exit 1
+}
 
-# execute the tests
+# execute the tests and store the result
 $ROOT/test.py; RESULT=$?
 
 # stop (and remove) the container
 for container in $(docker ps | grep ssh-server | cut -d' ' -f1); do
-  docker stop $container &>/dev/null
+  docker stop $container &>$TEMPLOG || {
+    echo "Error while stopping container $container:"
+    cat $TEMPLOG
+  }
 done
 
+#return the test result
 exit $RESULT
